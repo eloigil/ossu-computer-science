d7b4781eca33ad74fcdb27931f381191a9498f78
eloigil <eloigil6@gmail.com>
2019-05-20 00:22:02 +0200

    exercise 1a done + 1b half

---
+++ introduction-to-cs/6.00-introduction-to-computer-science-and-programming/02.core-elements-of-a-program/ps1a.py
@@ -0,0 +1,20 @@
+# Problem Set 1a
+# Name: Eloi Gil
+# Time Spent: 1:30
+#
+
+balance = float(input('balance: '))
+annualInterestRate = float(input('annual interest rate: '))
+minMonthlyPaymentRate = float(input('minimum monthly payment rate: '))
+
+month = 0.0
+while month < 12.0:
+    month += 1.0
+    print('Month ' + str(month))
+    minMonthlyPayment = round(balance * minMonthlyPaymentRate, 2)
+    print('minimum monthly payment: ' + str(minMonthlyPayment))
+    interestPaid = round(annualInterestRate / 12.0 * balance, 2)
+    principlePaid = round(minMonthlyPayment - interestPaid, 2)
+    print('principle paid: ' + str(principlePaid))
+    balance -= round(principlePaid, 2)
+    print('remaining balance: ' + str(round(balance, 2)))
--- introduction-to-cs/6.00-introduction-to-computer-science-and-programming/02.core-elements-of-a-program/ps1c.py
+++
@@ -1,7 +0,0 @@
-# Problem Set 1
-# Name: Jane Lee
-# Collaborators: John Doe
-# Time Spent: 3:30
-
-# @TODO
-
