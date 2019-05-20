4b92afdd19f7b4d2c6eb62978f23944c581214a6
eloigil <eloigil6@gmail.com>
2019-05-20 20:13:13 +0200

    exercise 1b

--- introduction-to-cs/6.00-introduction-to-computer-science-and-programming/02.core-elements-of-a-program/ps1b.py
+++ introduction-to-cs/6.00-introduction-to-computer-science-and-programming/02.core-elements-of-a-program/ps1b.py
@@ -2,3 +2,31 @@
 # Name: Eloi Gil
 # Time Spent: 1:30
 #
+
+initialBalance = float(input('Enter the outstanding balance on your credit card: '))
+annualInterestRate = float(input('Enter the annual credit card interest rate as a decimal: '))
+monthlyInterest = annualInterestRate / 12.0
+monthlyPayment = 10.0
+months = 1
+balance = initialBalance
+
+iterations = 0
+
+while balance > 0.0:
+    iterations += 1
+    months = 1
+    monthlyPayment += 10.0
+    balance = initialBalance
+    while months <= 12:
+        balance = balance * (1.0 + monthlyInterest) - monthlyPayment
+        if balance <= 0:
+            break
+        else:
+            months += 1
+            
+
+print(months)
+print(monthlyPayment)
+print(round(balance, 2))
+
+    
---
+++ introduction-to-cs/6.00-introduction-to-computer-science-and-programming/02.core-elements-of-a-program/ps1c.py
@@ -0,0 +1,4 @@
+# Problem Set 1b
+# Name: Eloi Gil
+# Time Spent: 1:30
+#
